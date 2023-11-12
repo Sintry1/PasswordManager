eace403b245a2e733142bc2bb85221613da0080b
Sintry1 <Dominic_smith2012@hotmail.co.uk>
2023-11-12 23:20:59 +0100

    I am going to kill myself soon

--- client/src/pages/PasswordVault.js
+++ client/src/pages/PasswordVault.js
@@ -1,31 +1,78 @@
 import { useEffect, useState } from "react";
 import "./PasswordVault.css";
+import Modal from "react-modal";
 
 export default function PasswordVault() {
-  const bcrypt = require("bcrypt");
+  const bcrypt = require("bcryptjs");
   let salt = "";
   const [site, setSite] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [generatedPassword, setGeneratedPassword] = useState("");
   const [passwordList, setPasswordList] = useState([]);
-  const [decryptedPasswordList, setDecryptedPasswordList] = useState([]);
+  const [decryptSite, setDecryptSite] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    loadPasswords();
+  }, []);
 
-  const hashMasterPassword = () => {
-    let password = prompt("Please enter your master password:");
-    const saltRounds = 10; // Specify the number of salt rounds
-    if (localStorage.getItem("Salt")) {
-      salt = localStorage.setItem("Salt");
-    } else {
-      salt = bcrypt.genSaltSync(saltRounds);
-      localStorage.setItem("Salt:", salt);
+  const loadPasswords = () => {
+    const storedPasswords = localStorage.getItem("passwordList");
+    const passwordList = storedPasswords ? JSON.parse(storedPasswords) : {};
+
+    // Reconstruct the proper structure
+    for (const site in passwordList) {
+      const entry = passwordList[site];
+      if (entry.iv && entry.password) {
+        entry.password = {
+          iv: new Uint8Array(entry.iv),
+          password: new Uint8Array(entry.password),
+        };
+      }
     }
 
-    const hashedPassword = bcrypt.hashSync(password, salt);
+    setPasswordList(passwordList);
+    console.log(passwordList);
+    return passwordList;
+  };
 
-    return hashedPassword;
+  const hashMasterPassword = async () => {
+    let masterPassword = prompt("Please enter your master password:");
+
+    if (!localStorage.getItem("Salt")) {
+      salt = crypto.getRandomValues(new Uint8Array(16));
+      localStorage.setItem("Salt", new TextDecoder().decode(salt));
+    } else {
+      salt = new TextEncoder().encode(localStorage.getItem("Salt"));
+    }
+
+    const encodedPassword = new TextEncoder().encode(masterPassword);
+
+    const keyMat = await crypto.subtle.importKey(
+      "raw",
+      encodedPassword,
+      { name: "PBKDF2" },
+      false,
+      ["deriveBits", "deriveKey"]
+    );
+
+    const derivedKey = await crypto.subtle.deriveKey(
+      {
+        name: "PBKDF2",
+        salt: salt,
+        iterations: 100000,
+        hash: { name: "SHA-256" },
+      },
+      keyMat,
+      { name: "AES-GCM", length: 256 },
+      true,
+      ["encrypt", "decrypt"]
+    );
+
+    // const exportedKey = await crypto.subtle.exportKey("raw", derivedKey);
+
+    return derivedKey;
   };
 
   const generateStrongPassword = (length = 32) => {
@@ -39,75 +86,296 @@ export default function PasswordVault() {
       const randomIndex = Math.floor(Math.random() * chars.length);
       password += chars.charAt(randomIndex);
     }
+    setPassword(password);
     return password;
   };
 
-  const loadPasswords = () => {
-    const storedPasswords = localStorage.getItem("passwordList")
-
-    const passwordList = storedPasswords ? JSON.parse(storedPasswords) : {};
-
-    return passwordList;
+  const clearAll = () => {
+    setModalOpen(true);
   };
 
-  const addPassword = (site, username, password) => {
-    const passwordList = loadPasswords();
-    if (passwordList[site]) {
+  const handleConfirm = () => {
+    localStorage.clear();
+    setModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setModalOpen(false);
+  };
+
+  const addPassword = async (site, username, password) => {
+    const masterKey = await hashMasterPassword();
+
+    // Retrieve the current password list
+    const currentPasswordList = { ...passwordList };
+
+    // Check if a password for the site already exists
+    if (currentPasswordList[site]) {
       console.log(`A password for ${site} already exists.`);
     } else {
-      const encryptedPassword = encryptInput(password);
-      passwordList[site] = {
+      const encryptedPassword = await encryptInput(password, masterKey);
+
+      // Add the new password entry to the list
+      currentPasswordList[site] = {
         site: site,
         username: username,
-        password: encryptedPassword,
+        password: {
+          iv: Array.from(encryptedPassword.iv),
+          password: Array.from(encryptedPassword.password),
+        }
       };
-      localStorage.setItem("passwordList", JSON.stringify(passwordList))
+
+      // Update the state with the new password list
+      setPasswordList(currentPasswordList);
+
+      // Update localStorage if necessary
+      localStorage.setItem("passwordList", JSON.stringify(currentPasswordList));
+
       console.log("Password successfully added.");
     }
   };
 
-  const encryptInput = (password) => {
-    const key = crypto
-      .pbkdf2Sync(hashMasterPassword(), localStorage.getItem("Salt"), 100000, 32, "sha256")
-      .toString("base64");
+  // const encryptInput = async (password) => {
+  //   const masterKey = await hashMasterPassword();
 
-    const iv = Buffer.from(crypto.randomBytes(16));
-    const cipher = crypto.createCipheriv(
-      "aes-256-ctr",
-      Buffer.from(key, "base64"),
-      iv
+  //   const keyMaterial = await crypto.subtle.exportKey("raw", masterKey);
+
+  //   const key = await crypto.subtle.deriveKey(
+  //     {
+  //       name: "PBKDF2",
+  //       salt: new TextEncoder().encode(localStorage.getItem("Salt")),
+  //       iterations: 100000,
+  //       hash: { name: "SHA-256" },
+  //     },
+  //     await crypto.subtle.importKey(
+  //       "raw",
+  //       keyMaterial,
+  //       { name: "PBKDF2" },
+  //       false,
+  //       ["deriveKey"]
+  //     ),
+  //     { name: "AES-GCM", length: 256 },
+  //     true,
+  //     ["encrypt", "decrypt"]
+  //   );
+
+  //   const iv = crypto.getRandomValues(new Uint8Array(16));
+  //   const cipher = await crypto.subtle
+  //     .encrypt(
+  //       { name: "AES-GCM", iv: iv },
+  //       key,
+  //       new TextEncoder().encode(password)
+  //     )
+  //     .catch((error) => {
+  //       console.error("Error during encryption:", error);
+  //       throw error;
+  //     });
+
+  //   // Log the encrypted password and IV to the console (for demonstration purposes)
+  //   console.log("Encrypted Password:", cipher);
+  //   console.log("IV:", iv);
+  //   return {
+  //     iv: iv,
+  //     password: new Uint8Array(cipher),
+  //   };
+  // };
+
+  const encryptInput = async (password, masterKey) => {
+    const keyMaterial = await crypto.subtle.exportKey("raw", masterKey);
+
+    console.log("Key material inside encryptInput:", keyMaterial);
+
+    const key = await crypto.subtle.deriveKey(
+      {
+        name: "PBKDF2",
+        salt: new TextEncoder().encode(localStorage.getItem("Salt")),
+        iterations: 100000,
+        hash: { name: "SHA-256" },
+      },
+      await crypto.subtle.importKey(
+        "raw",
+        keyMaterial,
+        { name: "PBKDF2" },
+        false,
+        ["deriveKey"]
+      ),
+      { name: "AES-GCM", length: 256 },
+      true,
+      ["encrypt", "decrypt"]
     );
 
-    const encryptedPassword = Buffer.concat([
-      cipher.update(password, "utf-8"),
-      cipher.final(),
-    ]);
+    const iv = crypto.getRandomValues(new Uint8Array(16));
+
+    const cipher = await crypto.subtle.encrypt(
+      { name: "AES-GCM", iv: iv },
+      key,
+      new TextEncoder().encode(password)
+    );
+
+    console.log("key inside encryptInput:", key);
+
+    console.log(
+      "Encrypted Password inside encryptInput:",
+      new Uint8Array(cipher)
+    );
+    console.log("IV inside encryptInput:", iv);
 
     return {
-      iv: iv.toString("hex"),
-      password: encryptedPassword.toString("hex"),
+      iv: iv,
+      password: new Uint8Array(cipher),
     };
   };
 
   // TESTED AND WORKING
-  const decryptInput = (encryption) => {
-    const key = crypto
-      .pbkdf2Sync(hashMasterPassword(), localStorage.getItem("Salt"), 100000, 32, "sha256")
-      .toString("base64");
+  // const decryptInput = async (encryption) => {
+  //   const masterKey = await hashMasterPassword();
 
-    const iv = Buffer.from(encryption.iv, "hex");
-    const decipher = crypto.createDecipheriv(
-      "aes-256-ctr",
-      Buffer.from(key, "base64"),
-      iv
-    );
+  //   const keyMaterial = await crypto.subtle.exportKey("raw", masterKey);
 
-    const decryptedPassword = Buffer.concat([
-      decipher.update(Buffer.from(encryption.password, "hex")),
-      decipher.final(),
-    ]);
+  //   try {
+  //     const key = await crypto.subtle.deriveKey(
+  //       {
+  //         name: "PBKDF2",
+  //         salt: new TextEncoder().encode(localStorage.getItem("Salt")),
+  //         iterations: 100000,
+  //         hash: { name: "SHA-256" },
+  //       },
+  //       await crypto.subtle.importKey(
+  //         "raw",
+  //         keyMaterial, // Use the key material directly
+  //         { name: "PBKDF2" },
+  //         false,
+  //         ["deriveKey"]
+  //       ),
+  //       { name: "AES-GCM", length: 256 },
+  //       true,
+  //       ["encrypt", "decrypt"]
+  //     );
 
-    return decryptedPassword.toString("utf-8");
+  //     // Convert password to Uint8Array
+  //     const passwordUint8Array = new Uint8Array(encryption.password);
+
+  //     const password = await crypto.subtle.decrypt(
+  //       {
+  //         name: "AES-GCM",
+  //         iv: encryption.iv,
+  //       },
+  //       key,
+  //       passwordUint8Array
+  //     );
+
+  //     // Log the decrypted password to the console (for demonstration purposes)
+  //     console.log("Decrypted Password:", new TextDecoder().decode(password));
+
+  //     return new TextDecoder().decode(password);
+  //   } catch (error) {
+  //     console.error("Error decrypting password:", error);
+  //     console.error("Full error object:", error);
+  //     console.error("Encryption object:", encryption);
+  //     throw error;
+  //   }
+  // };
+
+  const decryptInput = async (encryption, masterKey) => {
+    try {
+      console.log("Master Key inside decryptInput:", masterKey);
+
+      const keyMaterial = await crypto.subtle.exportKey("raw", masterKey);
+
+      console.log("Key Material inside decryptInput:", keyMaterial);
+
+      const key = await crypto.subtle.deriveKey(
+        {
+          name: "PBKDF2",
+          salt: new TextEncoder().encode(localStorage.getItem("Salt")),
+          iterations: 100000,
+          hash: { name: "SHA-256" },
+        },
+        await crypto.subtle.importKey(
+          "raw",
+          keyMaterial,
+          { name: "PBKDF2" },
+          false,
+          ["deriveKey"]
+        ),
+        { name: "AES-GCM", length: 256 },
+        true,
+        ["encrypt", "decrypt"]
+      );
+
+      console.log("Derived Key inside decryptInput:", key);
+
+      const passwordUint8Array = new Uint8Array(encryption.password);
+      console.log("Encryption.iv inside decryptInput", encryption.iv);
+      console.log(
+        "Encryption.password inside decryptInput",
+        passwordUint8Array
+      );
+
+      // Log the types and values of iv and password
+      console.log("Type of iv:", typeof encryption.iv);
+      console.log("Type of password:", typeof encryption.password);
+
+      // Log the values of iv and password
+      console.log("Value of iv:", encryption.iv);
+      console.log("Value of password:", passwordUint8Array);
+      const decryptedPassword = await crypto.subtle.decrypt(
+        {
+          name: "AES-GCM",
+          iv: encryption.iv,
+        },
+        key,
+        passwordUint8Array
+      );
+
+      console.log(
+        "Decrypted Password inside decryptInput:",
+        new TextDecoder().decode(decryptedPassword)
+      );
+
+      return new TextDecoder().decode(decryptedPassword);
+    } catch (error) {
+      console.error("Error during decryption:", error);
+      console.error("Error stack trace:", error.stack);
+      throw error; // Rethrow the error for further analysis
+    }
+  };
+
+  const decryptStoredPassword = async (site) => {
+    // Prompt the user for the master password
+
+    // Derive the master key
+    const masterKey = await hashMasterPassword();
+
+    // Retrieve the current password list
+    const currentPasswordList = { ...passwordList };
+
+    // Find the password entry for the specified site
+    const encryptedPasswordObject = currentPasswordList[site]?.password;
+
+    if (encryptedPasswordObject) {
+      const iv = new Uint8Array(encryptedPasswordObject.iv); // Convert iv to Uint8Array
+      const password = new Uint8Array(encryptedPasswordObject.password); // Convert password to Uint8Array
+
+      console.log(
+        "Encrypted Password Object in decryptStoredPassword:",
+        password
+      );
+
+      // Decrypt using the derived master key
+      const decryptedPassword = await decryptInput(
+        encryptedPasswordObject,
+        masterKey
+      );
+      console.log(
+        "Decrypted Password in decryptStoredPassword:",
+        decryptedPassword
+      );
+
+      return decryptedPassword;
+    } else {
+      alert(`No password found for ${site}`);
+    }
   };
 
   const handlePasswordChange = (e) => {
@@ -120,7 +388,7 @@ export default function PasswordVault() {
   return (
     <div className="Vault">
       <div>
-        <h1>Password vault for {username}</h1>
+        <h1>Password vault</h1>
       </div>
       <div className="Add">
         <h2>Add Password</h2>
@@ -130,64 +398,71 @@ export default function PasswordVault() {
           placeholder="Site/Platform"
           onChange={(e) => setSite(e.target.value)}
         />
-         <input
+        <input
           className="Input"
           type="text"
           placeholder="Username"
-          onChange={(e) => setSite(e.target.value)}
+          onChange={(e) => setUsername(e.target.value)}
         />
         <input
           className="Input"
           type="password"
           placeholder="Password"
-          value={generatedPassword || password}
+          value={password}
           onChange={handlePasswordChange}
         />
-        <button className="Button" onClick={generateStrongPassword}>
+        <button className="Button" onClick={() => generateStrongPassword()}>
           Generate a strong password
         </button>
-        <button className="Button" onClick={() => addPassword(site, username, password)}>
+        <button
+          className="Button"
+          onClick={() => addPassword(site, username, password)}
+        >
           Add Password
         </button>
       </div>
       <div>
-        <button className="Button" onClick={() => {}}>
-          Decrypt all Passwords
+        <h2>Input a password you would like to decrypt</h2>
+        <input
+          className="Input"
+          type="text"
+          placeholder="Site/Platform"
+          onChange={(e) => setDecryptSite(e.target.value)}
+        />
+        {/* <input
+          className="Input"
+          type="password"
+          placeholder="Decryption Password"
+          value={password}
+          onChange={() => {}}
+        /> */}
+        <button
+          className="Button"
+          onClick={() => decryptStoredPassword(decryptSite)}
+        >
+          Decrypt Password
         </button>
-        <ul>
-          {decryptedPasswordList.length > 0 ? (
-            decryptedPasswordList.map((entry, index) => (
-              <li key={index}>
-                Site: {entry.site}
-                <br />
-                Decrypted Password: {entry.password.decryptedPassword}
-              </li>
-            ))
-          ) : (
-            <li>
-              No decrypted passwords available. Click the button to decrypt.
-            </li>
-          )}
-        </ul>
       </div>
-      <div className="PasswordList">
-        <h1>Password List</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Site</th>
-              <th>Password</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.keys(passwordList).map((site, index) => (
-              <tr key={index}>
-                <td>{passwordList[site].site}</td>
-                <td>{passwordList[site].password.password}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div>
+        <button className="Button" onClick={clearAll}>
+          Clear All Passwords
+        </button>
+
+        <Modal
+          isOpen={modalOpen}
+          contentLabel="Confirmation Modal"
+          className="Modal"
+        >
+          <div>
+            <p>Are you sure you want to clear all passwords stored?</p>
+            <button className="Button" onClick={handleConfirm}>
+              Yes, clear it!
+            </button>
+            <button className="Button" onClick={handleCancel}>
+              Cancel
+            </button>
+          </div>
+        </Modal>
       </div>
     </div>
   );
